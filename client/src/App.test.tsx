053636be9d8b7ad33dffc15b39fc256c6fb6d04e
Carlos Toledo <CToledo9672@gmail.com>
2022-02-10 19:57:10 -0300

    no time for testing

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+const charactersList = ['Yoda', 'Darth Vader', 'Luke Skywalker', 'Leia', 'Han Solo', 'Boba Fett'];
 
-test('renders learn react link', () => {
+test('renders Star Wars', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElementStart = screen.getByText(/star/i);
+  const linkElementWars = screen.getByText(/wars/i);
+  expect(linkElementStart).toBeInTheDocument();
+  expect(linkElementWars).toBeInTheDocument();
+});
+
+
+test('renders Characters', () => {
+  render(<App />);
+  charactersList.map((e) => {
+    const linkElement = screen.getByText(e);
+    expect(linkElement).toBeInTheDocument();
+  })
+  
 });
